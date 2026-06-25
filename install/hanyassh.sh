6836d30d273168e8bb684fca97c015d999f3db6f
reyvoid09-ops <reyvoid09@gmail.com>
2026-06-25 08:04:59 +0700

    Update Git user in hanyassh.sh

--- install/hanyassh.sh
+++ install/hanyassh.sh
@@ -3,7 +3,7 @@
 # 2022
 # ==================================================
 
-GitUser="syapik96"
+GitUser="reyvoid09-ops"
 #wget https://github.com/${GitUser}/
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
